2c3b18ef43ef7f7f7c04589b22d72c33ca740a9f
Jethro Moey <jmse97@gmail.com>
2025-06-24 16:54:29 +0800

    Chapter 2(Exercise): Create a Configurable Component

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -11,7 +11,6 @@ export class UserComponent {
   @Input({ required: true }) avatar!: string;
   @Input({ required: true }) name!: string;
   @Output() select = new EventEmitter();
-  // select = output<string>();
 
   get imagePath() {
     return 'assets/users/' + this.avatar;
